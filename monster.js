354eb4a3e639b94537b2d8fbab23ca6ee7f3d6dd
AH RIAD <ahriad231@gmail.com>
2021-02-08 19:44:46 +0600

    after a long time hard working

--- monster.js
+++ monster.js
@@ -13,31 +13,38 @@ const displayFoods = foods =>{
         const foodDiv = document.createElement('div');
         foodDiv.className = 'single-result'
         foodDiv.innerHTML =`
-        <div onclick="displayFoodDetails('${food.strMeal}')">
+        <div onclick="displayFoodDetails(${food.idMeal})">
         <img src="${food.strMealThumb}">
         <h1>${food.strMeal} </h1>
         </div>`;
        foodContainer.appendChild(foodDiv);
-      
+
     });
 }
 const displayFoodDetails = name =>{ 
-const url = `https://www.themealdb.com/api/json/v1/1/list.php?i=${name}`
+const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${name}`
 fetch(url)
 .then(res => res.json())
 .then(data =>renderFoodInfo(data.meals));
-// .then (data => console.log(data.meals))
-
-
-
 }
 
 const renderFoodInfo = meal =>{
     // console.log(meal);
     const Div = document.getElementById('foodDetail');
-    Div.innerHTML =` <h1> ${meal.strMeal}</h1>
-       
+    Div.innerHTML =`
+    <img src =${meal[0].strMealThumb} <br>
+     <h1> ${meal[0].strMeal}</h1>
+     <h4> ${meal[0].strIngredient1} </h4>
+     <h4> ${meal[0].strIngredient2} </h4>
+     <h4> ${meal[0].strIngredient3} </h4>
+     <h4> ${meal[0].strIngredient4} </h4>
+     <h4> ${meal[0].strIngredient5} </h4>
+     <h4> ${meal[0].strIngredient6} </h4>
+     <h4> ${meal[0].strIngredient7} </h4>
+     <h4> ${meal[0].strIngredient8} </h4>
+     <h4> ${meal[0].strIngredient9} </h4>
+     <h4> ${meal[0].strIngredient10} </h4>
+
     `;
 }
-
 
