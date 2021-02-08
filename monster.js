0427ed1d4c9d0da2d2dd80e50cec792267530e53
AH RIAD <ahriad231@gmail.com>
2021-02-08 17:04:22 +0600

    last updated

--- monster.js
+++ monster.js
@@ -1,7 +1,7 @@
 
 const searchFoods = () =>{
     const searchText = document.getElementById('search-field').value;
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchText}`
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
     //load data
     fetch(url)
     .then (res => res.json())
@@ -25,27 +25,18 @@ const displayFoodDetails = name =>{
 const url = `https://www.themealdb.com/api/json/v1/1/list.php?i=${name}`
 fetch(url)
 .then(res => res.json())
-.then(data =>renderFoodInfo(data));
+.then(data =>renderFoodInfo(data.meals));
 // .then (data => console.log(data.meals))
 
 
 
 }
 
-const renderFoodInfo = food =>{
+const renderFoodInfo = meal =>{
+    // console.log(meal);
     const Div = document.getElementById('foodDetail');
-    Div.innerHTML =`<img src="${food.strMealThumb}">
-    <div>
-        <h4>${food.strMeal}</h4>
-        <h6>Ingredients</h6>
-        <p>${food.strIngredient1}</p>
-        <p>${food.strIngredient2}</p>
-        <p>${food.strIngredient3}</p>
-        <p>${food.strIngredient4}</p>
-        <p>${food.strIngredient5}</p>
-        <p>${food.strIngredient6}</p>
-        <p>${food.strIngredient7}</p>
-    </div>
+    Div.innerHTML =` <h1> ${meal.strMeal}</h1>
+       
     `;
 }
 
